61ef70ccf18b362386acdf85cfca39afcd0f7924
Owen Tsai <owentsai.v@gmail.com>
2019-07-28 23:30:32 +0800

    Add achievements in User Center

--- client/src/views/UCenter.vue
+++ client/src/views/UCenter.vue
@@ -28,11 +28,22 @@
             <el-tab-pane label="回答" name="my-answers"></el-tab-pane>
             <el-tab-pane label="群组" name="my-groups"></el-tab-pane>
           </el-tabs>
-          <router-view></router-view>
+          <router-view class="user-main-left"></router-view>
         </el-col>
         <el-col :span="8" class="hidden-md-and-down">
-          <el-card shadow="none">
+          <el-card shadow="none" class="u-center-side-card">
             <h4 style="text-align: center">成就 | ACHIEVEMENTS</h4>
+            <div class="medals">
+              <p><icon class="envision-xihuan"></icon><span> 221 次感谢</span></p>
+              <p><icon class="el-icon-coin"></icon><span> 1992 Envision积分</span></p>
+              <p><icon class="el-icon-date"></icon><span> 2019-01-01 加入</span></p>
+              <p><icon class="el-icon-collection"></icon><span> 12 创作数</span></p>
+              <hr>
+              <p><icon class="el-icon-medal-1"></icon><span> Envision 零号用户</span></p>
+              <p><icon class="el-icon-trophy"></icon><span> 人工智能技术协会第一届荣誉会员</span></p>
+              <p><icon class="el-icon-medal"></icon><span> Envision 初代贡献者</span></p>
+              <p><icon class="el-icon-medal"></icon><span> Envision 核心开发者者</span></p>
+            </div>
           </el-card>
         </el-col>
       </el-row>
@@ -106,5 +117,22 @@
   }
   .user-main {
     margin-top: 20px;
+  }
+  .medals p {
+    margin-bottom: 8px;
+    margin-top: 8px;
+  }
+  .medals icon {
+    vertical-align: middle;
+    font-size: 20px;
+  }
+  .medals span {
+    vertical-align: middle;
+    line-height: 20px;
+    font-size: 13px;
+  }
+  .user-main-left {
+    border: 1px solid #E4E7ED;
+    margin-top: -16px;
   }
 </style>
